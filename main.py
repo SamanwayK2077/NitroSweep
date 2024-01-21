9f45c6f36cbf975dc47686353b738fa53e7e7f52
Shadow <120722515+SamanwayK2077@users.noreply.github.com>
2024-01-21 23:06:46 +0530

    Update main.py
    Validation and updates done to main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from time import localtime, strftime, sleep
+from time import localtime, strftime
+import time
 from colorama import Fore
 import requests
 import random
 import string
+import os
 from pyfiglet import Figlet
 
 def print_ascii_art(text):
@@ -13,33 +15,29 @@ def print_ascii_art(text):
 print_ascii_art("NitroSweep")
 
 class NitroSweep:
-    def __init__(self, code_type: str, prox=None, codes=None):
-        self.type = code_type
-        self.codes = codes
-        self.proxies = prox
-        self.session = requests.Session()
-        self.prox_api = (
+    def __init__(this, code_type: str, prox=None, codes=None):
+        this.type = code_type
+        this.codes = codes
+        this.proxies = prox
+        this.session = requests.Session()
+        this.prox_api = (
             "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt"
         )
 
-    def __proxies__(self):
-        req = self.session.get(self.prox_api).text
-        if req is not None:
+    def __proxies__(this):
+        req = this.session.get(this.prox_api).text
+        if req != None:
             open("./data/proxies.txt", "a+").truncate(0)
             for proxy in req.split("\n"):
                 proxy = proxy.strip()
                 proxy = f"https://{proxy}"
                 open("./data/proxies.txt", "a").write(f"{proxy}\n")
 
-    def generate(self, scrape=None):
-        if scrape == "True" or (scrape is None and self.proxies is None):
-            self.__proxies__()
-
-        print_ascii_art("[NitroSweep] is ready to find codes.")
-
-        for _ in range(int(self.codes)):
+    def generate(this, scrape=None):
+        this.__proxies__()
+        for _ in range(int(this.codes)):
             try:
-                if self.proxies == "True":
+                if this.proxies == "True":
                     prox = {
                         "http": random.choice(
                             open("./data/proxies.txt", "r").read().splitlines()
@@ -48,7 +46,7 @@ class NitroSweep:
                 else:
                     prox = None
 
-                if self.type == "boost":
+                if this.type == "boost":
                     code = "".join(
                         [
                             random.choice(string.ascii_letters + string.digits)
@@ -62,33 +60,32 @@ class NitroSweep:
                             for i in range(16)
                         ]
                     )
-                req = self.session.get(
+                req = this.session.get(
                     f"https://discordapp.com/api/entitlements/gift-codes/{code}",
                     proxies=prox,
                     timeout=10,
                 ).status_code
-
                 if req == 200:
                     print(
-                        f"{Fore.GREEN}[NitroSweep] discord.gift/{code} | {Fore.GREEN}Valid code"
-                    )
+                        f"{Fore.GREEN}[NitroSweep] discord.gift/{code} | valid")
+                    print(f"{Fore.MAGENTA}[NitroSweep] Found in "+ (time.time() - stime)+" seconds.")
                     open("./data/valid.txt", "a").write(f"{code}\n")
-                elif req == 404:
+                if req == 404:
                     print(
-                        f"{Fore.RED}[NitroSweep] discord.gift/{code} | {Fore.RED}Invalid code"
+                        f"{Fore.RED}[NitroSweep] discord.gift/{code} | invalid"
                     )
-                elif req == 429:
+
+                if req == 429:
                     print(
-                        f"{Fore.YELLOW}[NitroSweep] discord.gift/{code} | {Fore.YELLOW}Rate Limited"
+                        f"{Fore.YELLOW}[NitroSweep] discord.gift/{code} | ratelimited"
                     )
 
             except Exception as e:
                 print(f"{Fore.RED}[NitroSweep] {e}")
 
         print(
-            f"{Fore.LIGHTMAGENTA_EX}[NitroSweep] NitroSweep has successfully checked {self.codes} codes."
+            f"{Fore.LIGHTMAGENTA_EX}[NitroSweep] Successfully checked {this.codes} codes."
         )
-        sleep(1.5)
 
 if __name__ == "__main__":
     while True:
@@ -96,7 +93,9 @@ if __name__ == "__main__":
             f"{Fore.LIGHTMAGENTA_EX}[NitroSweep] Code Type (boost, classic): "
         )
         prox=True
-
-        codes = input(f"{Fore.LIGHTMAGENTA_EX}[NitroSweep] Number of codes to test (Higher number=Higher chance to find \nanswer but means higher time): "
+        scrape_proxy=True
+        codes = input(
+            f"{Fore.LIGHTMAGENTA_EX}[NitroSweep] Number of codes to sweep: "
         )
-        NitroSweep(code_type, prox, codes).generate()
+        NitroSweep(code_type, prox, codes).generate(scrape=scrape_proxy)
+        stime=time.time()
